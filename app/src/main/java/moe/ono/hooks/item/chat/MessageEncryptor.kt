f8aa95383a3976c54236226b347e74f0ac36330b
cwuom <cwuomcwuom@163.com>
2025-05-18 16:32:22 +0800

    opt(MessageEncryptor): update function description

--- app/src/main/java/moe/ono/hooks/item/chat/MessageEncryptor.kt
+++ app/src/main/java/moe/ono/hooks/item/chat/MessageEncryptor.kt
@@ -27,7 +27,7 @@ import moe.ono.util.Logger
 import moe.ono.util.SyncUtils
 
 @SuppressLint("DiscouragedApi")
-@HookItem(path = "聊天与消息/群聊加密消息", description = "发送的消息将加密抄送，仅针对群聊\n开启此功能需要重启生效。\n* 禁止用于非法用途，违者后果自负")
+@HookItem(path = "聊天与消息/群聊加密消息", description = "发送的消息将加密抄送，仅针对群聊\n* 禁止用于非法用途，违者后果自负")
 class MessageEncryptor : BaseClickableFunctionHookItem() {
     override fun entry(classLoader: ClassLoader) {
         QQHookCodec.hijackers.add(object: IHijacker {
