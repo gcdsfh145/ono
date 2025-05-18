2a048cab123789d552f21a06e6c20d70aeb13743
cwuom <cwuomcwuom@163.com>
2025-05-18 17:21:28 +0800

    feat(MsgPush): support message decryption

--- app/src/main/java/moe/ono/hooks/base/api/QQOnMsfPush.kt
+++ app/src/main/java/moe/ono/hooks/base/api/QQOnMsfPush.kt
@@ -1,15 +1,32 @@
 package moe.ono.hooks.base.api
 
+import com.google.protobuf.UnknownFieldSet
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam
+import kotlinx.io.core.ByteReadPacket
+import kotlinx.io.core.readBytes
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.protobuf.ProtoBuf
+import moe.ono.config.ConfigManager
+import moe.ono.constants.Constants
+import moe.ono.ext.getUnknownObject
+import moe.ono.ext.getUnknownObjects
 import moe.ono.hooks._base.ApiHookItem
+import moe.ono.hooks._base.BaseHookItem
 import moe.ono.hooks._core.annotation.HookItem
+import moe.ono.hooks._core.factory.HookItemFactory
 import moe.ono.hooks.item.chat.HoldRevokeMessageCore
+import moe.ono.hooks.item.chat.MessageEncryptor
 import moe.ono.hooks.item.chat.SelfMessageReactor
 import moe.ono.hooks.item.entertainment.BlockBadlanguage
 import moe.ono.hooks.item.entertainment.DoNotBrushMeOff
+import moe.ono.hooks.protocol.entries.Message
+import moe.ono.hooks.protocol.entries.MessagePush
+import moe.ono.hooks.protocol.entries.TextMsgExtPbResvAttr
 import moe.ono.reflex.ClassUtils
 import moe.ono.reflex.MethodUtils
-import moe.ono.util.Logger
+import moe.ono.util.AesUtils
 import moe.ono.util.QAppUtils
 import top.artmoe.inao.entries.InfoSyncPushOuterClass
 import top.artmoe.inao.entries.MsgPushOuterClass
@@ -42,7 +59,7 @@ class QQOnMsfPush : ApiHookItem() {
         }
     }
 
-    private fun handleInfoSyncPush(buffer: ByteArray, param: XC_MethodHook.MethodHookParam) {
+    private fun handleInfoSyncPush(buffer: ByteArray, param: MethodHookParam) {
         val infoSyncPush = InfoSyncPushOuterClass.InfoSyncPush.parseFrom(buffer)
         infoSyncPush.syncRecallContent.syncInfoBodyList.forEach { syncInfoBody ->
             syncInfoBody.msgList.forEach { qqMessage ->
@@ -64,7 +81,9 @@ class QQOnMsfPush : ApiHookItem() {
         }
     }
 
-    private fun handleMsgPush(buffer: ByteArray, param: XC_MethodHook.MethodHookParam) {
+    @OptIn(ExperimentalSerializationApi::class)
+    private fun handleMsgPush(buffer: ByteArray, param: MethodHookParam) {
+        val newMsgPush = ProtoBuf.decodeFromByteArray<MessagePush>(buffer)  // TODO: 只保留 newMsgPush
         val msgPush = MsgPushOuterClass.MsgPush.parseFrom(buffer)
         val msg = msgPush.qqMessage
 //         if (msgTargetUid != EnvHelper.getQQAppRuntime().currentUid) return  //  不是当前用户接受就返回
@@ -85,6 +104,8 @@ class QQOnMsfPush : ApiHookItem() {
                     if (msgPush.qqMessage.messageHead.senderPeerId == QAppUtils.getCurrentUin().toLong()) {
                         SelfMessageReactor().react(msgPush.qqMessage.messageHead.senderInfo.peerId, msgPush.qqMessage.messageContentInfo.msgSeq.toLong())
                     }
+                    val msgBody = newMsgPush.msgBody
+                    tryDecryptMsg(param, msgBody)
                 }
             }
 
@@ -97,4 +118,63 @@ class QQOnMsfPush : ApiHookItem() {
             }
         }
     }
+
+    private fun tryDecryptMsg(param: MethodHookParam, msgBody: Message) {
+        val encryptKey = ConfigManager.dGetString(
+            Constants.PrekCfgXXX + HookItemFactory.getItem(MessageEncryptor::class.java).path,
+            "ono"
+        )
+        if (encryptKey.isBlank()) {
+            return
+        }
+        val encryptedText = msgBody.body?.richMsg?.elems?.firstOrNull { it.text != null } ?: return
+        val resvBuffer = encryptedText.text?.resv ?: return
+        kotlin.runCatching {
+            val encryptMsg = ProtoBuf.decodeFromByteArray<TextMsgExtPbResvAttr>(resvBuffer).wording ?: return
+            if (encryptMsg.size <= 8) return // 不是加密消息
+            val aesKey = AesUtils.md5(encryptKey)
+            val encryptBuffer = ByteReadPacket(encryptMsg)
+            if(0x114514 != encryptBuffer.readInt()) return // 不是加密消息
+            if(encryptKey.hashCode() != encryptBuffer.readInt()) return // 密钥不匹配
+            val decryptMsg = AesUtils.aesDecrypt(encryptBuffer.readBytes(), aesKey)
+            val decryptMsgBody = UnknownFieldSet.parseFrom(decryptMsg)
+            val decryptRichText = decryptMsgBody.getUnknownObject(1)
+
+            val oldMsgPush = UnknownFieldSet.parseFrom(param.args[1] as ByteArray)
+            val oldMsg = oldMsgPush.getUnknownObject(1)
+            val oldMsgBody = oldMsg.getUnknownObject(3)
+            val oldRichText = oldMsgBody.getUnknownObject(1)
+
+            val newRichText = UnknownFieldSet.newBuilder(decryptRichText)
+            val newElements = oldRichText.getUnknownObjects(2).mapNotNull {
+                if (!it.hasField(1) && !it.hasField(6)) it else null
+            }
+            newRichText.mergeField(2, UnknownFieldSet.Field.newBuilder().also { field ->
+                newElements.forEach { field.addLengthDelimited(it.toByteString()) }
+            }.build())
+
+            val newMsgBody = UnknownFieldSet.newBuilder(oldMsgBody)
+            newMsgBody.clearField(1)
+            newMsgBody.addField(1, UnknownFieldSet.Field.newBuilder().also { field ->
+                field.addLengthDelimited(newRichText.build().toByteString())
+            }.build())
+
+            val newMsg = UnknownFieldSet.newBuilder(oldMsg)
+            newMsg.clearField(3)
+            newMsg.addField(3, UnknownFieldSet.Field.newBuilder().also { field ->
+                field.addLengthDelimited(newMsgBody.build().toByteString())
+            }.build())
+
+            val newMsgPush = UnknownFieldSet.newBuilder(oldMsgPush)
+            newMsgPush.clearField(1)
+            newMsgPush.addField(1, UnknownFieldSet.Field.newBuilder().also { field ->
+                field.addLengthDelimited(newMsg.build().toByteString())
+            }.build())
+
+            //PlatformTools.copyToClipboard(text = "Decrypt:" + newMsgPush.build().toByteArray().toHexString())
+
+            param.args[1] = newMsgPush.build().toByteArray()
+        }
+    }
+
 }
---
+++ app/src/main/java/moe/ono/hooks/protocol/entries/MessagePush.kt
@@ -0,0 +1,59 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+package moe.ono.hooks.protocol.entries
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.protobuf.ProtoNumber
+import moe.ono.ext.EMPTY_BYTE_ARRAY
+
+@Serializable
+data class MessagePush(
+    @ProtoNumber(1) var msgBody: Message
+)
+
+@Serializable
+data class Message(
+    @ProtoNumber(1) var msgHead: MessageHead,
+    @ProtoNumber(2) var content: MessageContentInfo,
+    @ProtoNumber(3) var body: MessageBody? = null
+)
+
+@Serializable
+data class MessageHead(
+    @ProtoNumber(1) val peerId: UInt = UInt.MIN_VALUE,
+    @ProtoNumber(2) val peerUid: String? = null,
+    @ProtoNumber(5) val targetId: Long = Long.MIN_VALUE,
+    @ProtoNumber(6) val targetUid: String? = null
+)
+
+@Serializable
+data class MessageContentInfo(
+    @ProtoNumber(1) val msgType: Int = Int.MIN_VALUE,
+    @ProtoNumber(2) val msgSubType: Int = Int.MIN_VALUE,
+    @ProtoNumber(3) val subType: Int = Int.MIN_VALUE,
+    @ProtoNumber(5) val msgSeq: Int = Int.MIN_VALUE,
+    @ProtoNumber(6) val msgTime: Long = Long.MIN_VALUE,
+    @ProtoNumber(12) val msgUid: Long = Long.MIN_VALUE,
+)
+
+@Serializable
+data class MessageBody(
+    @ProtoNumber(1) val richMsg: NTRichText? = null,
+    @ProtoNumber(2) val msgContent: ByteArray? = EMPTY_BYTE_ARRAY,
+)
+
+@Serializable
+data class NTRichText(
+    @ProtoNumber(2) val elems: ArrayList<Elem>? = null
+)
+
+@Serializable
+data class Elem(
+    @ProtoNumber(1) val text: Text? = null,
+)
+
+@Serializable
+data class Text(
+    @ProtoNumber(1) val text: String? = null,
+    @ProtoNumber(12) val resv: ByteArray? = null
+)
